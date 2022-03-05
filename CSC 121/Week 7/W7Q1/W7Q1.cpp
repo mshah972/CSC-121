df8ffcf1cf4dd35ec1dc7c8f3629adec0e0df969
Moksh Shah <93416417+mshah972@users.noreply.github.com>
2022-03-04 19:05:31 -0600

    CSC 121

--- CSC 121/Week 7/W7Q1/W7Q1.cpp
+++ CSC 121/Week 7/W7Q1/W7Q1.cpp
@@ -1,8 +1,22 @@
 /*
 
     Week 7      Dicussion Question 1
-    Moksh Shah  02/28/2021
-    Protected Data
+    Moksh Shah  03/04/2021
+    Random #'s
     CSC 121
 
 */
+
+#include <iostream>
+#include <random>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+   
+    int num1, num2, num3, num4;
+    default_random_engine engine{ static_cast<unsigned int>(time(0)) };
+
+
+}
