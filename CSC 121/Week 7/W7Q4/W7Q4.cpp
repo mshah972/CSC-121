3b7b61fe9a098cc013998f38733413d476596a60
Moksh Shah <93416417+mshah972@users.noreply.github.com>
2022-03-08 12:15:35 -0600

    CSC 121

--- CSC 121/Week 7/W7Q4/W7Q4.cpp
+++ CSC 121/Week 7/W7Q4/W7Q4.cpp
@@ -14,22 +14,39 @@ using namespace std;
 
 int main()
 {
-    string name;
+    string name = " ";
     double bill = 0, tax = 0.19, totalBill = 0;
 
-    cout << "Please enter the name of the bill ('q' to exit): ";
-    cin >> name;
-    cout << endl;
+   vector<string> vname;
+   vector<double> vbill;
     
     while (name != "q")
     {
         cout << "Please enter the name of the bill ('q' to exit): ";
         cin >> name;
+        vname.push_back(name);
         cout << endl;
+
+        if(name != "q")
+        {
+            cout << "Please enter the amount of the bill: ";
+            cin >> bill;
+            vbill.push_back(bill);
+            cout << endl;
+            totalBill += bill;
+        }
+
     }
     
+    cout << endl;
+    cout << "\"Your total amount without tax is \" : " << totalBill << "\n";
+    cout << endl;
+    
+    totalBill = totalBill + (totalBill * tax);
 
+    cout << "\"Your total amount with tax is \" : " << totalBill << "\n";
+    cout << endl;
 
-
+    
     return 0;
 }
