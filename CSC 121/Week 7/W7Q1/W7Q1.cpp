a655145e406bcd7f98761aab165a04ef4f8ececc
Moksh Shah <93416417+mshah972@users.noreply.github.com>
2022-03-08 12:57:41 -0600

    CSC 121

--- CSC 121/Week 7/W7Q1/W7Q1.cpp
+++ CSC 121/Week 7/W7Q1/W7Q1.cpp
@@ -13,7 +13,6 @@
 #include <string.h>
 using namespace std;
 
-int num1, num2, num3, num4;
 
 int main()
 {
@@ -21,5 +20,34 @@ int main()
     default_random_engine engine{ static_cast<unsigned int>(time(0)) }; 
 
     uniform_int_distribution<unsigned int> randNum{ 1, 100 };
-
+    
+    cout << "\nLets Start with Loop 1.\n" << endl;
+    
+    for(int i = 1; i <= 20; i++)                                                        // for loop for loop 1
+    {
+        cout << "Loop 1: " << randNum(engine) << endl;                                  // generates 20 random numbers from 1 - 100
+    }
+    
+    cout << "\nLets Start Loop 2 now.\n" << endl;
+    
+    for(int j = 1; j <= 12; j++)                                                        // for loop for loop 2
+    {
+        cout << "Loop 2: " << randNum(engine) << endl;                                  // generates 12 random numbers from 1 - 100
+    }
+    
+    cout << "\nLets move on to Loop 3.\n" << endl; 
+    
+    for(int k = 1; k <= 6; k++)                                                         // for loop for loop 3
+    {
+        cout << "Loop 3: " << randNum(engine) << endl;                                  // generates 6 random numbers from 1 - 100
+    }
+    
+    cout << "\nNow lets do the last Loop 4.\n" << endl;
+    
+    for(int m = 1; m <= 3; m++)                                                         // for loop for loop 4
+    {
+        cout << "Loop 4: " << randNum(engine) << endl;                                  // generates 3 random numbers from 1 - 100
+    }
+    
+    return 0;
 }
