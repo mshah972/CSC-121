2f323d58d4e2682ba0b0a029af9d9559e4f12d32
Moksh Shah <93416417+mshah972@users.noreply.github.com>
2022-03-16 19:54:20 -0500

    CSC 121

--- CSC 121/Week 8/W8Q4/W8Q4.cpp
+++ CSC 121/Week 8/W8Q4/W8Q4.cpp
@@ -9,3 +9,28 @@
 
 #include <iostream>
 using namespace std;
+
+int fac(int);                                                                                               // initializing function fac
+
+int main()                                                                                                  // main function
+{
+    
+    for(int i = 1; i < 11; i++)                                                                             // for loop to print 1 to 10 for factorials
+    {
+        cout << "Factorials for " << i << " is " <<  fac(i) << endl;                                        // printing the factorials
+    }
+
+    return 0;
+}
+
+int fac(int n)                                                                                              // function fac
+{
+    if(n == 0)                                                                                              // if statement with a condition n = 0
+    {
+        return 1;                                                                                           // return 1
+    }
+    else                                                                                                    // or
+    {
+        return n * fac(n - 1);                                                                              // return the factorials by using recursion
+    }
+}
