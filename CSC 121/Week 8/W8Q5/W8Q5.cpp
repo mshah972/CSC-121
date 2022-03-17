0c9fd4e3afd9e8fa323c3eec11c0ee7a7c59f726
Moksh Shah <93416417+mshah972@users.noreply.github.com>
2022-03-16 21:13:31 -0500

    CSC 121

--- CSC 121/Week 8/W8Q5/W8Q5.cpp
+++ CSC 121/Week 8/W8Q5/W8Q5.cpp
@@ -9,3 +9,38 @@
 
 #include <iostream>
 using namespace std;
+
+int xvalue(int);                                                                                    // initializing xvalue function
+int yreference(int*);                                                                               // initializing yreference function
+
+int main()                                                                                          // main function
+{
+    int x, y;                                                                                       // int variable for x and y
+
+    cout << endl;
+    cout << "Please enter x number: ";                                                              // prompt for x
+    cin >> x;
+    cout << endl;
+
+    cout << "Please enter y number: ";                                                              // prompt for y
+    cin >> y;
+    cout << endl;
+
+    cout << "x * x = " << xvalue(x) << endl;                                                             // multiplying x with x and calling xvalue function
+
+    cout << "y * y = " << yreference(&y) << endl;                                                        // multiplying y with y and calling yreference function
+
+    cout << endl;
+
+    return 0;
+}
+
+int xvalue(int n)                                                                                   // function xvalue with parameter int
+{
+    return n = n * n;                                                                               // multiplying input number with same number
+}
+
+int yreference(int* n)                                                                              // function yreference with parameter int
+{
+    return *n = *n * *n;
+}
