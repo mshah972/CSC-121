24c204c37dd0efdef33f934b68fbe0c6a94a4784
smoksh17 <73042339+smoksh17@users.noreply.github.com>
2022-03-18 16:21:47 -0500

    Week 9

--- CSC 121/Week 9/W9Q2/W9Q2.cpp
+++ CSC 121/Week 9/W9Q2/W9Q2.cpp
@@ -2,7 +2,7 @@
 
     Week 9      Dicussion Question 2
     Moksh Shah  03/14/2021
-    Enumerations 
+    Enumerations
     CSC 121
 
 */
@@ -13,40 +13,45 @@
 #include <stdio.h>
 using namespace std;
 
-enum rgb {r = 255, g = 255, b = 255};
-
-int main()
+enum rgb                                                                                                        // enumeration for rgb preset to black
 {
-    rgb Red = r, Green = g, Blue = b;
-    int random;
-    string input = "", black = "";
+    r = 255,
+    g = 255,
+    b = 255
+}; 
+
+int main()                                                                                                      // main function
+{
+    rgb Red = r, Green = g, Blue = b;                                                                           // declaration of enum rgb with variable for 3 color set to the preset
+    int random;                                                                                                 // int random to generate a random number
+    string input = "", black = "";                                                                              // string variable for inputs
 
     cout << endl;
-    cout << "Do you want to generate a random rgb color? (y/n): ";
+    cout << "Do you want to generate a random rgb color? (y/n): ";                                              // prompt
     cin >> input;
     cout << endl;
 
-    cout << "Are you looking for a black color? (y/n): ";
+    cout << "Are you looking for a black color? (y/n): ";                                                       // prompt
     cin >> black;
 
-    if(black == "y" || black == "Y")
+    if (black == "y" || black == "Y") // if statement to check if the user want a black color
     {
         cout << endl;
-        cout << "\nYour black color code is R:" << Red << " G:" << Green << " B:" << Blue << endl;
-        cout << "\n\nDo you want to generate another color code? (y/n): ";
+        cout << "\nYour black color code is R:" << Red << " G:" << Green << " B:" << Blue << endl;              // print out the preset color codes
+        cout << "\n\nDo you want to generate another color code? (y/n): ";                                      // ask the user is they want any other color codes
         cin >> input;
-        cout << endl; 
+        cout << endl;
     }
-        
-    while(input == "y" )
-    {    
+
+    while (input == "y")                                                                                        // while loop if input = "y"
+    {
         cout << endl;
 
-        for (int i = 0; i < 1; i++)
+        for (int i = 0; i < 1; i++)                                                                             // for loop to print out those random codes
         {
-            printf("\nYour random generated color code is R:");
+            printf("\nYour random generated color code is R:");                                             
 
-            for (int j = 0; j < 1; j++)
+            for (int j = 0; j < 1; j++)                                                                         // for loop to print a random number from 1 to 256 for red
             {
                 random = 1 + (rand() % 256);
                 Sleep(700);
@@ -55,7 +60,7 @@ int main()
 
             printf(" G:");
 
-            for (int j = 0; j < 1; j++)
+            for (int j = 0; j < 1; j++)                                                                         // for loop to print a random number from 1 to 256 for green
             {
                 random = 1 + (rand() % 256);
                 Sleep(700);
@@ -64,7 +69,7 @@ int main()
 
             printf(" B:");
 
-            for (int j = 0; j < 1; j++)
+            for (int j = 0; j < 1; j++)                                                                         // for loop to print a random number from 1 to 256 fro blue
             {
                 random = 1 + (rand() % 256);
                 Sleep(700);
@@ -73,19 +78,19 @@ int main()
             break;
         }
         cout << endl;
-        cout << "\n\nDo you want to generate another color code? (y/n): ";
+        cout << "\n\nDo you want to generate another color code? (y/n): ";                                      // prompting the user if they want another color code
         cin >> input;
     }
-    
-    if(input == "n")
+
+    if (input == "n")                                                                                           // if input = "n" then exit the program
     {
-        cout << "No worries, you can use our program any time you need to get a random rgb code." << endl;
+        cout << "\nNo worries, you can use our program any time you need to get a random rgb code." << endl;
     }
-   
-    cout << "\nThanks and have great day!!\n" << endl;
+
+    cout << "\nThanks and have great day!!\n";
     exit;
 
-    Red = r;
+    Red = r;                                                                                                    // setting the red, green, blue variable to preset
     Green = g;
     Blue = b;
 
