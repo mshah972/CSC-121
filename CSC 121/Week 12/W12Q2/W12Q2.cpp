64241fc90275291216dbe5db31bcf248067e0357
smoksh17 <73042339+smoksh17@users.noreply.github.com>
2022-04-15 19:22:57 -0500

    Week 12 Question 2

---
+++ CSC 121/Week 12/W12Q2/W12Q2.cpp
@@ -0,0 +1,21 @@
+/*
+
+    Moksh Shah      Discussion Question 1
+    Week 12         04/15/2022
+    CSC 121
+    Methods
+
+*/
+
+#include <iostream>
+#include <string>
+#include "methods.h"                                                        // include methods.h
+using namespace std;
+
+int main()
+{
+    methods w12q2;                                                          // create object w12q2 of class methods
+    w12q2.Display();                                                        // call method Display                          
+
+    return 0;
+}
---
+++ CSC 121/Week 12/W12Q2/methods.h
@@ -0,0 +1,29 @@
+/*
+
+    Moksh Shah      Discussion Question 1
+    Week 12         04/15/2022
+    CSC 121
+    Class
+
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class methods {                                                                     // class methods
+    public:                                                                         // public access specifier
+        string name;                                                                // string variable name                                 
+        int age;                                                                    // integer variable age
+
+        void Display() {                                                            // void method Display                              
+            cout << "\nEnter your name: ";                                          // prompt user to enter name
+            cin >> name;                                                            // input name                               
+            cout << "\nEnter your age: ";                                           // prompt user to enter age                    
+            cin >> age;                                                             // input age
+
+            cout << "\nName: " << name << endl;                                     // display name
+            cout << "Age: " << age << endl;                                         // display age
+            cout << endl;                                                           // end line                                       
+        }
+};
